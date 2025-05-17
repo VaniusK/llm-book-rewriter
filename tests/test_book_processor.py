c7faf6dc1e33743f030e0eec7e0b8ea68f2f31b8
Ivan Konobeitsev <konobeitsev3@gmail.com>
2025-05-17 19:38:53 +0500

    update tests

--- tests/test_book_processor.py
+++ tests/test_book_processor.py
@@ -3,37 +3,45 @@ from unittest.mock import patch
 from book_processor import BookProcessor
 from config import config
 from heuristic_applier import HeuristicApplier
+from unittest.mock import patch
 
-@patch.dict(config, {'processing': {'output_dir': 'test_output'}}, clear=True)
-@patch('book_processor.FileHandler')
-@patch('book_processor.LLM')
 class TestBookProcessor(unittest.TestCase):
+
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}}, clear=True)
+    @patch("book_processor.FileHandler")
+    @patch("book_processor.LLM")
     def test_split_into_chunks(self, MockLLM, MockFileHandler):
-        processor = BookProcessor("google", "fb2")
+        processor = BookProcessor("google", "fb2", "result.fb2")
         self.assertEqual(processor.split_into_chunks("0123456789>", 10), ["0123456789>"])
         self.assertEqual(processor.split_into_chunks("0123456789", 10), ["0123456789"])
         self.assertEqual(processor.split_into_chunks("0123456789>0123456789>", 10), ["0123456789>", "0123456789>"])
         self.assertEqual(processor.split_into_chunks("0123456789.0123456789", 10), ["0123456789.", "0123456789"])
         self.assertEqual(processor.split_into_chunks("0123456789!0123456789", 10), ["0123456789!", "0123456789"])
 
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}}, clear=True)
+    @patch("book_processor.FileHandler")
+    @patch("book_processor.LLM")
     def test_validate_response(self, MockLLM, MockFileHandler):
-        processor = BookProcessor("google", "fb2")
+        processor = BookProcessor("google", "fb2", "result.fb2")
         self.assertEqual(processor.validate_response("<p>Hi<p/>", "<p>Hi<p/>"), True)
         self.assertEqual(processor.validate_response("<p>Hi<p/>", "<p>Hi<p/"), False)
         self.assertEqual(processor.validate_response("<p>Hi<p/>", "<p>Hi"), False)
 
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}}, clear=True)
+    @patch("book_processor.FileHandler")
+    @patch("book_processor.LLM")
     def test_format_response(self, MockLLM, MockFileHandler):
-        processor = BookProcessor("google", "fb2")
+        processor = BookProcessor("google", "fb2", "result.fb2")
         self.assertEqual(processor.format_response("<p> Let's go for a dog\t   <p/>", "<p>Let's go for a walk<p/>"),"<p> Let's go for a walk\t   <p/>")
         self.assertEqual(processor.format_response("\n\nSome text\n\n", "\t New text \t"),"\n\nNew text\n\n")
 
-    def test_heuristics(self, MockLLM, MockFileHandler):
+    def test_heuristics(self):
         heuristic_applier = HeuristicApplier()
         self.assertEqual(heuristic_applier.preprocessing_remove_commas("Apples, bananas, oranges"), ["Apples bananas oranges", {}])
-        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text <br>"), ["@ text @", {"replaced_tags": ["br", "br"], "placeholder": "@"}])
-        self.assertEqual(heuristic_applier.postprocessing_replace_tags_with_placeholder("@ text @", {"replaced_tags": ["br", "br"], "placeholder": "@"}), "<br> text <br>")
-        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text@ <br>"), ["# text@ #", {"replaced_tags": ["br", "br"], "placeholder": "#"}])
-        self.assertEqual(heuristic_applier.postprocessing_replace_tags_with_placeholder("# text@ #", {"replaced_tags": ["br", "br"], "placeholder": "#"}), "<br> text@ <br>")
+        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text <br>"), ["<image/> text <image/>", {"replaced_tags": ["<br>", "<br>"], "placeholder": "<image/>"}])
+        self.assertEqual(heuristic_applier.postprocessing_replace_tags_with_placeholder("<image/> text <image/>", {"replaced_tags": ["<br>", "<br>"], "placeholder": "<image/>"}), "<br> text <br>")
+        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text <image/> <br>"), ["@ text @ @", {"replaced_tags": ["<br>", "<image/>", "<br>"], "placeholder": "@"}])
+        self.assertEqual(heuristic_applier.postprocessing_replace_tags_with_placeholder("@ text @ @", {"replaced_tags": ["<br>", "<image/>", "<br>"], "placeholder": "@"}), "<br> text <image/> <br>")
 
 
 if __name__ == '__main__':
