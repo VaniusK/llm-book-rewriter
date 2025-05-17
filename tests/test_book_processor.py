b45d8305c276c05e26a91ee304be846156abd892
Ivan Konobeitsev <konobeitsev3@gmail.com>
2025-05-17 20:27:27 +0500

    update tests

--- tests/test_book_processor.py
+++ tests/test_book_processor.py
@@ -7,7 +7,7 @@ from unittest.mock import patch
 
 class TestBookProcessor(unittest.TestCase):
 
-    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}}, clear=True)
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}, "heuristics": {"placeholder": "<image/>"}}, clear=True)
     @patch("book_processor.FileHandler")
     @patch("book_processor.LLM")
     def test_split_into_chunks(self, MockLLM, MockFileHandler):
@@ -18,7 +18,7 @@ class TestBookProcessor(unittest.TestCase):
         self.assertEqual(processor.split_into_chunks("0123456789.0123456789", 10), ["0123456789.", "0123456789"])
         self.assertEqual(processor.split_into_chunks("0123456789!0123456789", 10), ["0123456789!", "0123456789"])
 
-    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}}, clear=True)
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}, "heuristics": {"placeholder": "<image/>"}}, clear=True)
     @patch("book_processor.FileHandler")
     @patch("book_processor.LLM")
     def test_validate_response(self, MockLLM, MockFileHandler):
@@ -27,21 +27,21 @@ class TestBookProcessor(unittest.TestCase):
         self.assertEqual(processor.validate_response("<p>Hi<p/>", "<p>Hi<p/"), False)
         self.assertEqual(processor.validate_response("<p>Hi<p/>", "<p>Hi"), False)
 
-    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}}, clear=True)
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}, "heuristics": {"placeholder": "<image/>"}}, clear=True)
     @patch("book_processor.FileHandler")
     @patch("book_processor.LLM")
     def test_format_response(self, MockLLM, MockFileHandler):
         processor = BookProcessor("google", "fb2", "result.fb2")
         self.assertEqual(processor.format_response("<p> Let's go for a dog\t   <p/>", "<p>Let's go for a walk<p/>"),"<p> Let's go for a walk\t   <p/>")
         self.assertEqual(processor.format_response("\n\nSome text\n\n", "\t New text \t"),"\n\nNew text\n\n")
+        self.assertEqual(processor.format_response("<body> content <body/>", "\n  <body> content2 <body/>  \n"),"<body> content2 <body/>")
 
+    @patch.dict(config, {"processing": {"output_dir": "test_output", "workers_amount": 3}, "heuristics": {"placeholder": "<image/>"}}, clear=True)
     def test_heuristics(self):
         heuristic_applier = HeuristicApplier()
         self.assertEqual(heuristic_applier.preprocessing_remove_commas("Apples, bananas, oranges"), ["Apples bananas oranges", {}])
-        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text <br>"), ["<image/> text <image/>", {"replaced_tags": ["<br>", "<br>"], "placeholder": "<image/>"}])
+        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text <br>"), [" <image/>  text  <image/> ", {"replaced_tags": ["<br>", "<br>"], "placeholder": "<image/>"}])
         self.assertEqual(heuristic_applier.postprocessing_replace_tags_with_placeholder("<image/> text <image/>", {"replaced_tags": ["<br>", "<br>"], "placeholder": "<image/>"}), "<br> text <br>")
-        self.assertEqual(heuristic_applier.preprocessing_replace_tags_with_placeholder("<br> text <image/> <br>"), ["@ text @ @", {"replaced_tags": ["<br>", "<image/>", "<br>"], "placeholder": "@"}])
-        self.assertEqual(heuristic_applier.postprocessing_replace_tags_with_placeholder("@ text @ @", {"replaced_tags": ["<br>", "<image/>", "<br>"], "placeholder": "@"}), "<br> text <image/> <br>")
 
 
 if __name__ == '__main__':
