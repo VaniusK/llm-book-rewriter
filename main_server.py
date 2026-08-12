2e54bc8cf2d33cde06b72162a0811d7531ce83f0
konobeitsev3 <konobeitsev3@gmail.com>
2026-08-12 12:29:21 +0300

    feat: progress

--- main_server.py
+++ main_server.py
@@ -1,3 +1,4 @@
+from file_handler import FileHandler
 from starlette.staticfiles import StaticFiles
 import logging
 import asyncio
@@ -11,6 +12,7 @@ from pathlib import Path
 import json
 from book_processor import BookProcessor
 from config import load_config, deep_merge_dicts, config_local_filename
+import difflib
 
 
 logging.basicConfig(
@@ -64,7 +66,7 @@ async def process_file(config_str: str = Form(...), file: UploadFile = File(...)
         book_processor = BookProcessor(config, ext)
         input_file = INPUT_DIR / Path(id + "." + ext)
         output_file = Path(f"{id}_rewritten." + ext)
-        tasks[id] = (asyncio.create_task(book_processor.process_book(input_file, output_file)), input_file, output_file, original_filename, file.filename)
+        tasks[id] = (asyncio.create_task(book_processor.process_book(input_file, output_file)), input_file, output_file, original_filename, file.filename, config)
     return {"message": "Начата обработка файла", "task_id": id}
 
 @app.get("/tasks/{task_id}")
@@ -93,5 +95,32 @@ async def get_task_result(task_id: str, background_tasks: BackgroundTasks):
         filename=tasks[task_id][4],
         media_type="application/zip"
     )
+
+@app.get("/tasks/{task_id}/diff")
+async def get_task_diff(task_id: str):
+    if not task_id in tasks:
+        raise HTTPException(status_code=404, detail="Задача не найдена")
+    if not tasks[task_id][0].done():
+        raise HTTPException(status_code=400, detail="Задача ещё не завершена")
+    
+    file_handler = FileHandler(tasks[task_id][1].suffix[1:], tasks[task_id][5]).file_handler
+    original_text = file_handler.extract_text(tasks[task_id][1])
+    processed_text = file_handler.extract_text(OUTPUT_DIR / tasks[task_id][2])
+    matcher = difflib.SequenceMatcher(isjunk=None, a=original_text, b=processed_text, autojunk=True)
+
+    diff_list = []
+    for tag, i1, i2, j1, j2 in matcher.get_opcodes():
+        if tag == 'equal':
+            continue 
+            
+        diff_item = {
+            "operation": tag,
+            "source_range": (i1, i2),
+            "target_range": (j1, j2),
+            "text_removed": original_text[i1:i2],
+            "text_added": processed_text[j1:j2]
+        }
+        diff_list.append(diff_item)
+    return {"diff_list": diff_list, "original_text": original_text, "processed_text": processed_text}
 
 app.mount("/", StaticFiles(directory="static", html=True), name="static")
